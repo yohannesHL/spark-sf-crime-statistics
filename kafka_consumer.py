54dadd0705ef54a2614a4db7b8e9f36dfdd2f31e
Yohannes Libanos <yhlibanos@gmail.com>
2020-06-25 14:25:17 +0100

    Clean up kafka consumer

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -4,22 +4,25 @@ import json
 import logging
 import kafka
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger = logging.getLogger('KafkaConsumer')
 
-KAFKA_BROKER_URL = os.getenv('KAFKA_BROKER_URL', 'localhost:9092')
-KAFKA_TOPIC_NAME = os.getenv('KAFKA_TOPIC_NAME', 'org.sanfrancisco.crime.police-calls')
+KAFKA_BROKER_URL = os.getenv('KAFKA_BROKER_URL')
+KAFKA_TOPIC_NAME = os.getenv('KAFKA_TOPIC_NAME')
 
-class Consumer():
-    '''
-        Kafka Consumer 
-        
-    '''
-    def __init__(self, topic_name, message_handler, **kwargs):
-        self.topic_name = topic_name
+class KafkaConsumer():
+    '''Consumes events from a kafka topic and processes the data via a custom message_handler
+    
+    Arguments:
+        topic {string} -- Topic name to send kafka event to
+        message_handler {Callable} -- A callback function to that will process incomming messages. 
+            Expects positional arguments: `key` and `value`.
+        **kwargs -- Additional keyword arguments passed to kafka.KafkaConsumer
+    '''  
+    def __init__(self, topic, message_handler, **kwargs):
+        self.topic_name = topic
         self.handle_message = message_handler
         self.consumer = kafka.KafkaConsumer(
-            topic_name,
+            topic,
             bootstrap_servers = KAFKA_BROKER_URL,
             client_id = 'spark_kafka_sink',
             group_id = 'cg_spark_kafka_sink',
@@ -27,7 +30,7 @@ class Consumer():
             value_deserializer = self.deserialize_json,
             **kwargs
         )    
-        logger.info('Kafka consumer subscribed to topic: %s', topic_name)
+        logger.info(f'Kafka consumer subscribed to topic: {topic}')
 
     def deserialize_json(self, value):
         return json.loads(value)
@@ -36,14 +39,16 @@ class Consumer():
         
         for message in self.consumer:
             try:
-                self.handle_message(message.value)
-            except Exception as e:
-                logging.error('Error unable to handle message : %', e)
+                self.handle_message(message.key, message.value)
+            except Exception as error:
+                logging.error(error)
 
 
 if __name__ == '__main__':
     logging.basicConfig()
-    logger.debug('Starting Kafka Consumer')
+    logger.setLevel(logging.DEBUG)
+
+    logger.debug('Starting KafkaConsumer')
     
-    c = Consumer(KAFKA_TOPIC_NAME, lambda x: print(x))
+    c = KafkaConsumer(KAFKA_TOPIC_NAME, lambda k,v: print(k, v))
     c.consume()
